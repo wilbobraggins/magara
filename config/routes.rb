193c390d1f60c06f27c940564421d98890362b51
wilbobraggins <wilbobraggins@gmail.com>
2018-12-07 03:55:25 +0000

    Created User controller and view page and updated routes to reflect that

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
   root to: 'high_voltage/pages#show', id: 'home'
   resources :houses
+  resources :users, only: :show
 end
